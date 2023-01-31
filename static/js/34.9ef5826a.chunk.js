fb5e12e7ee89075284d624a97ee1b7469831ddf4
NataliShkarupeta <113308128+NataliShkarupeta@users.noreply.github.com>
2023-01-31 15:17:35 +0000

    Deploying to gh-pages from @ NataliShkarupeta/goit-react-hw-05@e358d60cbfe486253335f33d73fde5d59732b452 🚀

---
+++ static/js/34.9ef5826a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[34],{34:function(n,e,t){t.r(e);var a=t(406),r=t(184);e.default=function(){return(0,r.jsx)(a.a,{children:" Ops......No info on this path!"})}},406:function(n,e,t){t.d(e,{a:function(){return c}});var a,r=t(168),c=t(444).ZP.h1(a||(a=(0,r.Z)(["\npadding: 40px;\ntext-align: center;\n"])))}}]);
+//# sourceMappingURL=34.9ef5826a.chunk.js.map
