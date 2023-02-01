cae4db81edf163f95b6a2ea153051c53bb2a9061
Nataliia Shkarupeta <natalinardi.kh@gmail.com>
2023-02-01 11:44:48 +0200

    add fixes

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,12 +1,10 @@
-import { fetchApiCast } from "service/service"
-import { useParams} from 'react-router-dom';
+import { fetchApiCast } from 'service/service';
+import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
-
-const Cast =()=>{
-   const { movieId } = useParams();
-   const [actors, setActors] = useState([]);
-
+const Cast = () => {
+  const { movieId } = useParams();
+  const [actors, setActors] = useState([]);
 
   useEffect(() => {
     fetchApiCast(movieId)
@@ -19,18 +17,22 @@ const Cast =()=>{
       .then(data => setActors(data.cast))
       .catch(error => console.log(error));
   }, [movieId]);
-if (!actors){
-  return null
-} 
-return actors.map(({ character, original_name, profile_path, id }) => (
-  <li key={id}>
-    <img
-      src={`https://image.tmdb.org/t/p/w200/${profile_path}`}
-      alt={original_name}
-    />
-    <p>{original_name}</p>
-    <p>Character: {character}</p>
-  </li>
-));
-}
+
+
+  return (
+    <>
+      {actors.length === 0 && <p>We don`t have information about the cast</p>}
+      {actors.map(({ character, original_name, profile_path, id }) => (
+        <li key={id}>
+          <img
+            src={`https://image.tmdb.org/t/p/w200/${profile_path}`}
+            alt={original_name}
+          />
+          <p>{original_name}</p>
+          <p>Character: {character}</p>
+        </li>
+      ))}
+    </>
+  );
+};
 export default Cast;
